b9e6400cb49bbdcb6e4eb860bd23edffe69ad4df
Jaikanth J <jaikanthjay46@gmail.com>
2023-07-02 22:13:19 +0530

    feat: target macos universal only

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// #[cfg(target_os = "macos")]
-// #[macro_use]
-// extern crate objc;
+#[cfg(target_os = "macos")]
+#[macro_use]
+extern crate objc;
 
 mod macos;
 mod spotlight;
